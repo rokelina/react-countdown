9b1c7cef7b6bc221ded320939fc6c0bf8850a98c
Rosina Scampino <rosinascampino@gmail.com>
2023-01-14 19:50:20 -0500

    adds reset button, changes hook to useState

--- src/counter.js
+++ src/counter.js
@@ -1,19 +1,32 @@
-import { useReducer, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 
 function Countdown(){
-    const [counter, setCounter] = useReducer((counter) => (counter===0 ? counter : counter-1), 10)
+    const [counter, setCounter] = useState(10);
 
-    useEffect(() => {document.title = `${counter} times to go!`}, [counter]);
-
+    const countdownHandle = () => {
+        setCounter((counter) => counter > 0 ? counter - 1 : 0);
+    };
+    
+    const resetHandle = () => {setCounter(10)};
+    
+    useEffect(() => {document.title = `${counter} clicks to go!`}, [counter]);
+    
     return (
         <div className="counter-container">
             <div>
                 <h3>{ counter }</h3>
-                <p>{counter} times to go!</p>
+                <p>{counter} clicks to go!</p>
             </div>
             <div>
-                <button onClick={setCounter}>Countdown</button>
+                <button 
+                className="countdown-button"
+                onClick={countdownHandle}>Countdown</button>
+            </div>
+            <div>
+                <button 
+                className="reset-button"
+                onClick={resetHandle}>Reset</button>
             </div>
         </div>
     )
